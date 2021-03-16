6f9940a6a0c6ac3b2b83df0938e52ff1620f8ac4
klementj <klementj@gmail.com>
2021-03-16 21:49:00 +0100

    Start animations when sections is in viewport

---
+++ assets/js/init.js
@@ -0,0 +1,26 @@
+const services = document.querySelectorAll('.services-section__grid')
+let currentSlide = 0
+
+async function nextSlide() {
+  services[currentSlide].classList.add("after-active")
+  await sleep(1000)
+  services[currentSlide].classList.remove("active")
+  services[currentSlide].classList.remove("after-active")
+  currentSlide = (currentSlide + 1) % services.length
+  services[currentSlide].classList.add("active")
+}
+
+let isServiceShown = false
+
+document.addEventListener(
+  'scroll',
+  () => {
+    if (isVisible(services[0]) && !isServiceShown) {
+      isServiceShown = true
+      services[currentSlide].classList.add("active")
+      setInterval(nextSlide, 10000)
+    }
+  },
+  { passive: true }
+);
+
